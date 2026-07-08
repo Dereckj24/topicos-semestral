aae383fc199ccefaeed1ad550da2ffd48b12ac68
Dereckj24 <dereckmorenob2442@gmail.com>
2026-07-08 01:28:05 -0500

    df

--- app.py
+++ app.py
@@ -16,7 +16,13 @@ if 'GOOGLE_APPLICATION_CREDENTIALS_JSON' in st.secrets:
     # 2. Convertirlo en diccionario válido de Python
     creds_dict = json.loads(creds_string)
     
-    # 3. Autenticar usando las credenciales nativas de Google OAuth2
+    # --- LA LÍNEA MÁGICA SALVADORA ---
+    # Forzar a Python a interpretar las barras invertidas como saltos de línea reales de criptografía
+    if 'private_key' in creds_dict:
+        creds_dict['private_key'] = creds_dict['private_key'].replace('\\n', '\n')
+    # ---------------------------------
+    
+    # 3. Autenticar usando las credenciales limpias de Google OAuth2
     scopes = ['https://www.googleapis.com/auth/earthengine', 'https://www.googleapis.com/auth/cloud-platform']
     credentials = service_account.Credentials.from_service_account_info(creds_dict, scopes=scopes)
     
